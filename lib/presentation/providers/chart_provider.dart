3f0625c180539ab871cdee739ad819923808cb3c
Dubem Ezeagwu <dubemezeagwu@gmail.com>
2026-05-17 14:36:20 -0400

    refactor: added ref for tracking state

--- lib/presentation/providers/chart_provider.dart
+++ lib/presentation/providers/chart_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../core/models/chart_data_point.dart';
+import 'performance_provider.dart';
 
 /// Maximum number of data points to keep in buffer.
 /// Allows panning back through historical data.
@@ -42,7 +43,9 @@ class ChartState {
 
 /// Notifier managing chart data state.
 class ChartNotifier extends StateNotifier<ChartState> {
-  ChartNotifier() : super(const ChartState.initial());
+  final Ref ref;
+
+  ChartNotifier(this.ref) : super(const ChartState.initial());
 
   /// Adds a new data point to the chart.
   ///
@@ -100,6 +103,9 @@ class ChartNotifier extends StateNotifier<ChartState> {
       dataPoints: updatedPoints,
       nextSampleIndex: currentIndex,
     );
+
+    // Track chart render for performance metrics
+    ref.read(performanceProvider.notifier).recordChartRender();
   }
 
   /// Clears all chart data (e.g., on disconnect or new session).
@@ -110,5 +116,5 @@ class ChartNotifier extends StateNotifier<ChartState> {
 
 /// Provider for chart data state.
 final chartProvider = StateNotifierProvider<ChartNotifier, ChartState>((ref) {
-  return ChartNotifier();
+  return ChartNotifier(ref);
 });
