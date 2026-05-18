acd5afcf99af493950c6c88f795dab3431a77a00
Dubem Ezeagwu <dubemezeagwu@gmail.com>
2026-05-18 15:36:52 -0400

    chore: removed foundation.dart import on camera service and provider

--- lib/presentation/providers/camera_provider.dart
+++ lib/presentation/providers/camera_provider.dart
@@ -1,5 +1,4 @@
 import 'package:camera/camera.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../services/camera_service.dart';
 
